aa0114d89543600710e942e84df5f7a2a239530d
woonhahaha <woonhahaha@gmail.com>
2020-09-28 22:08:03 +0900

    save model every 5 epoch and check test accuracy during training

--- feature_extraction_val2017.py
+++ feature_extraction_val2017.py
@@ -11,12 +11,14 @@ import pandas as pd
 from skimage import io
 from PIL import Image
 from torch.utils.data import Dataset, Subset, DataLoader  # Gives easier dataset managment and creates mini batches
+import time
 
 from sklearn.model_selection import train_test_split
 
 from tensorboardX import SummaryWriter
+import matplotlib.pyplot as plt
 
-class PlaceDataset(Dataset):
+class TrainDataset(Dataset):
     def __init__(self, csv_file, img_dir, transform=None):
         self.annotations = pd.read_csv(csv_file)
         self.img_dir = img_dir
@@ -29,15 +31,66 @@ class PlaceDataset(Dataset):
         # img_path = os.path.join(self.img_dir, str(self.annotations.iloc[index, 0]) + '.jpg')
         img_path = os.path.join(self.img_dir, str(self.annotations.iloc[index, 0]))
         image = io.imread(img_path)
-        image = Image.fromarray(image)
+        image = Image.fromarray(image).convert('RGB')
         y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
         # print(img_path)
+        if self.transform:
+            image = self.transform(image)
 
+        return (image, y_label)
+class PlaceDataset(Dataset):
+    def __init__(self, csv_file, img_dir, transform=None):
+        self.annotations = pd.read_csv(csv_file)
+        self.img_dir = img_dir
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.annotations)
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.img_dir, str(self.annotations.iloc[index, 0]) + '.jpg')
+        # img_path = os.path.join(self.img_dir, str(self.annotations.iloc[index, 0]))
+        image = io.imread(img_path)
+        image = Image.fromarray(image).convert('RGB')
+        y_label = torch.tensor(int(self.annotations.iloc[index, 1]))
+        # print(img_path)
         if self.transform:
             image = self.transform(image)
 
         return (image, y_label)
 
+def save_model(model_name, epoch, model):
+    dir_name = model_name + time.strftime('-%Y%m%d-%H%M%S', time.localtime(time.time()))
+    checkpoint_path = os.path.join('checkpoints', dir_name)
+    if not os.path.exists(checkpoint_path):
+        print('creating dir {}'.format(checkpoint_path))
+        os.mkdir(checkpoint_path)
+
+    checkpoint_file_path = os.path.join(checkpoint_path, 'epoch-{}.pkl'.format(epoch))
+    print('==> Saving checkpoint ... epoch {}'.format(epoch))
+    torch.save(model, checkpoint_file_path)
+
+# Check accuracy on training to see how good our model is
+def check_accuracy(loader, model, mode, epoch):
+    num_correct = 0
+    num_samples = 0
+    model.eval()
+
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device=device)
+            y = y.to(device=device)
+
+            scores = model(x)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == y).sum()
+            num_samples += predictions.size(0)
+        # print(num_correct/num_samples)
+        print("-------"+mode+"-------")
+        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct.item() / num_samples) * 100:.2f}')
+        print(num_correct.item())
+        writer.add_scalar(mode + 'accuracy', float(num_correct.item() / num_samples) * 100, epoch)
+    model.train()
 
 # Set device
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -46,7 +99,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 in_channel = 3
 num_classes = 3
 learning_rate = 1e-3
-batch_size = 32
+batch_size = 128
 num_epochs = 200
 
 def set_parameter_requires_grad(model, feature_extracting):
@@ -64,21 +117,37 @@ transform_train = transforms.Compose([
     transforms.ToTensor(),
     transforms.Normalize(rgb_mean, rgb_std),
 ])'''
-transform = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor()])
-train_set = PlaceDataset(csv_file='data/csv/gt5000.csv', img_dir='./dataset/images/val2017',
+transform = transforms.Compose([transforms.Resize((224, 224)), transforms.RandomHorizontalFlip(), transforms.ToTensor()])
+train_set = TrainDataset(csv_file='data/csv/gt5000.csv', img_dir='./dataset/images/val2017',
                              transform=transform)
 test_set = PlaceDataset(csv_file='data/csv/movie_gt3.csv', img_dir='data/test3',
                              transform=transform)
+
+### show train images
+# fig = plt.figure()
+# for i in range(len(train_set)):
+#     sample = train_set[i]
+#     print(i, sample[0].shape)
+#     ax = plt.subplot(2, 5, i + 1)
+#     plt.tight_layout()
+#     ax.set_title('Sample #{}'.format(i))
+#     ax.axis('off')
+#     plt.imshow(  sample[0].permute(1, 2, 0)  )
+#     plt.pause(0.001)
+#     if i == 9:
+#         plt.show()
+#         break
+
 print(len(train_set), len(test_set))
 # train_set, test_set = torch.utils.data.random_split(dataset, [400, 100])
 # train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=100, shuffle=False)
 # train_set = Subset(dataset, train_idx)
 # test_set = Subset(dataset, test_idx)
-train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(dataset=train_set, batch_size=batch_size, shuffle=True) ##True
 test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
 
 # Model
-model_conv = torchvision.models.resnet101() # resnet101-fc.in_features #pretrained=True
+model_conv = torchvision.models.resnet101(pretrained=True) # resnet101-fc.in_features
 # print(model_conv)
 # for param in model_conv.parameters():
 #     param.requires_grad = False
@@ -103,6 +172,7 @@ for epoch in range(num_epochs):
     losses = []
 
     for batch_idx, (data, targets) in enumerate(train_loader):
+        # print(data.shape)
         # Get data to cuda if possible
         data = data.to(device=device)
         targets = targets.to(device=device)
@@ -123,32 +193,19 @@ for epoch in range(num_epochs):
     print(f'Cost at epoch {epoch} is {sum(losses) / len(losses)}')
     if epoch % 5 == 0:
         writer.add_scalar('train_loss', sum(losses) / len(losses), epoch)
+        # writer.add_scalar('test_loss', )
+        check_accuracy(test_loader, model_conv, "Test", epoch)
+        # save checkpoints
+        save_model(model_conv.__class__.__name__, epoch, model_conv)
 
-# Check accuracy on training to see how good our model is
-def check_accuracy(loader, model):
-    num_correct = 0
-    num_samples = 0
-    model.eval()
 
-    with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
 
-            scores = model(x)
-            _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
-            num_samples += predictions.size(0)
-        print(num_correct/num_samples)
-        print(f'Got {num_correct} / {num_samples} with accuracy {float(num_correct) / float(num_samples) * 100:.2f}')
-        writer.add_scalar('accuracy', float(num_correct) / float(num_samples) * 100, num_correct / num_samples)
-    model.train()
 
 
 print("Checking accuracy on Training Set")
-check_accuracy(train_loader, model_conv)
+check_accuracy(train_loader, model_conv, "Train", 200)
 
 print("Checking accuracy on Test Set")
-check_accuracy(test_loader, model_conv)
+check_accuracy(test_loader, model_conv, "Test", 200)
 
 writer.close()
