9dfe63f19d85271db8672482ef0306ed4e90ab35
woonhahaha <woonhahaha@gmail.com>
2020-11-16 20:12:01 +0900

    places365 indoor 3

--- feature_extraction_places365.py
+++ feature_extraction_places365.py
@@ -1,5 +1,4 @@
 # reference : https://tutorials.pytorch.kr/beginner/transfer_learning_tutorial.html
-# AlexNet
 import torch
 import torch.nn as nn  # All neural network modules, nn.Linear, nn.Conv2d, BatchNorm, Loss functions
 import torch.optim as optim  # For all Optimization algorithms, SGD, Adam, etc.
@@ -7,12 +6,7 @@ from torch.optim import lr_scheduler
 import torchvision.transforms as transforms  # Transformations we can perform on our dataset
 import torchvision
 import os
-import pandas as pd
-from skimage import io
-from PIL import Image
-from torch.utils.data import Dataset, Subset, DataLoader  # Gives easier dataset managment and creates mini batches
-
-from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader  # Gives easier dataset management and creates mini batches
 
 from tensorboardX import SummaryWriter
 import time
@@ -21,16 +15,24 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 # Hyperparameters
 in_channel = 3
-num_classes = 4
+num_classes = 3
 learning_rate = 1e-3
-batch_size = 64
+batch_size = 128
 num_epochs = 200
+
+dir_made = 0 # flag for checkpoint dir
+checkpoint_path = ""
 def save_model(model_name, epoch, model):
-    dir_name = model_name + time.strftime('-%Y%m%d-%H%M%S', time.localtime(time.time()))
-    checkpoint_path = os.path.join('checkpoints', dir_name)
-    if not os.path.exists(checkpoint_path):
-        print('creating dir {}'.format(checkpoint_path))
-        os.mkdir(checkpoint_path)
+
+    global dir_made
+    global checkpoint_path
+    if dir_made != 1:
+        dir_name = time.strftime('%Y%m%d-%H%M%S-', time.localtime(time.time())) + model_name
+        checkpoint_path = os.path.join('checkpoints', dir_name)
+        if not os.path.exists(checkpoint_path):
+            print('creating dir {}'.format(checkpoint_path))
+            os.mkdir(checkpoint_path)
+            dir_made = 1
 
     checkpoint_file_path = os.path.join(checkpoint_path, 'epoch-{}.pkl'.format(epoch))
     print('==> Saving checkpoint ... epoch {}'.format(epoch))
@@ -65,8 +67,8 @@ def set_parameter_requires_grad(model, feature_extracting):
 # Load Data
 transform = transforms.Compose([transforms.Resize((299, 299)), transforms.RandomHorizontalFlip(), transforms.ToTensor()])
 
-train_set = torchvision.datasets.ImageFolder(root="dataset/MITPlaces_indoor/train", transform=transform)
-test_set = torchvision.datasets.ImageFolder(root="dataset/MITPlaces_indoor/test", transform=transform)
+train_set = torchvision.datasets.ImageFolder(root="dataset/MITPlaces_indoor_3/train", transform=transform)
+test_set = torchvision.datasets.ImageFolder(root="dataset/MITPlaces_indoor_3/test", transform=transform)
 # print(dataset.__getitem__(18))
 # train_set, test_set = torch.utils.data.random_split(dataset, [900, 100])
 # train_idx, test_idx = train_test_split(list(range(len(dataset))), test_size=1000, shuffle=False)
@@ -124,6 +126,7 @@ test_loader = DataLoader(dataset=test_set, batch_size=batch_size, shuffle=False)
 # model_conv = model_conv.to(device)
 
 ### Inception-v3 ### input size: 299x299
+model_name = "Inception-v3"
 model_conv = torchvision.models.inception_v3(pretrained=True)
 for param in model_conv.parameters():
      param.requires_grad = False
@@ -139,7 +142,7 @@ optimizer = optim.Adam(model_conv.fc.parameters(), lr=learning_rate)
 exp_lr_scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
 # tensorboard
-writer = SummaryWriter(comment=model_conv.__class__.__name__)
+writer = SummaryWriter(comment=model_name)
 
 # Train Network
 for epoch in range(num_epochs):
@@ -173,12 +176,12 @@ for epoch in range(num_epochs):
         optimizer.step()
 
     print(f'Cost at epoch {epoch} is {sum(losses) / len(losses)}')
-    if epoch % 5 == 0:
+    if epoch % 100 == 99:
         writer.add_scalar('train_loss', sum(losses) / len(losses), epoch)
         # writer.add_scalar('test_loss', )
         check_accuracy(test_loader, model_conv, "Test", epoch)
         # save checkpoints
-        save_model(model_conv.__class__.__name__, epoch, model_conv)
+        save_model(model_name, epoch + 1, model_conv)
 
 
 
